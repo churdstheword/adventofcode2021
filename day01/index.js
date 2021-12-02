953446a5814372c34bc578cb0546f750d8a90db2
Richard Lovern <churdstheword@gmail.com>
2021-12-01 22:46:01 -0500

    Finished solution for Day 1

--- day01/index.js
+++ day01/index.js
@@ -2,28 +2,44 @@
 
 import fs from "fs";
 import path from "path";
+import readline from "readline";
 
-async function* fileLineIterator(readable) {
-    let previous = '';
-    for await (const chunk of readable) {
-        previous += chunk;
-        let lines = previous.split(/\r?\n/);
-        while (lines.length > 1) {
-            let line = String(lines.shift());
-            if (line.length > 0) {
-                yield line;
+class Solution {
+
+    constructor(lines) {
+        this.lines = lines;
+    }
+
+    solvePartOne() {
+        let count = 0;
+        for (let i = 1; i < this.lines.length; i++) {
+            if (this.lines[i] - this.lines[i - 1] > 0) {
+                count++;
             }
         }
-        previous = lines.join('');
+        return count;
     }
-    if (previous.length > 0) {
-        yield previous;
+
+    solvePartTwo() {
+        let count = 0;
+        let prev = Number(this.lines[0]) + Number(this.lines[1]) + Number(this.lines[2]);     
+        for (let i = 1; i < this.lines.length - 2; i++) {
+            let curr = Number(this.lines[i]) + Number(this.lines[i + 1]) + Number(this.lines[i + 2]);
+            if (curr - prev > 0) {
+                count++;
+            }
+            prev = curr
+        }
+        return count;
     }
+
 }
 
-const input = fs.createReadStream(path.resolve('input.txt'));
-const reader = fileLineIterator(input);
+let lines = [];
+const readable = fs.createReadStream(path.resolve('input.txt'));
+const fileReader = readline.createInterface({ input: readable, crlfDelay: Infinity })
+for await (const line of fileReader) lines.push(line);
 
-for await (const line of reader) {
-    console.log(line);
-}
+const solution = new Solution(lines);
+console.log('Part 1:', solution.solvePartOne())
+console.log('Part 2:', solution.solvePartTwo())
