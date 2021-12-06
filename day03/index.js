6ac2fb7740ffc8b832ea9abfa08d9d9c164853e2
Richard Lovern <churdstheword@gmail.com>
2021-12-05 23:36:31 -0500

    Adding solution for Day 3, part 2

--- day03/index.js
+++ day03/index.js
@@ -10,36 +10,82 @@ class Solution {
         this.lines = lines;
     }
 
-    calculateGammaRate(report) {
-        let sum = 0;
-        for (const [index, value] of report.reverse().entries()) {
-            let digit = (value <= 0) ? 0 : 1;
-            sum += digit * Math.pow(2, index);
+    calculateGammaRate(lines) {
+        let rate = 0;
+        for (let col = 0; col < lines[0].length; col++) {
+            const column = this.fetchColumnFromRows(lines, col);
+            const mcb = this.getMostCommonBit(column)
+            rate += mcb * (Math.pow(2, lines[0].length - (col + 1)));
         }
-        return sum
+        return rate;
     }
 
-    calculateEpsilonRate(report) {
-        let sum = 0;
-        for (const [index, value] of report.reverse().entries()) {
-            let digit = (value <= 0) ? 1 : 0;
-            sum += digit * Math.pow(2, index);
+    calculateEpsilonRate(lines) {
+        let rate = 0;
+        for (let col = 0; col < lines[0].length; col++) {
+            const column = this.fetchColumnFromRows(lines, col);
+            const lcb = this.getLeastCommonBit(column);
+            rate += lcb * Math.pow(2, lines[0].length - (col + 1));
         }
-        return sum
+        return rate;
+    }
+
+    calculateCO2ScrubberRaiting(lines) {
+        let raiting = '';
+        for (let col = 0; lines.length > 1; col++) {
+            const column = this.fetchColumnFromRows(lines, col);
+            const lcb = this.getLeastCommonBit(column);
+            lines = lines.filter(this.columnHasCommonBit(col, lcb))
+            if (lines.length == 1) {
+                raiting = parseInt(lines[0], 2);
+                break;
+            }
+        }
+        return raiting;
+    }
+
+    calculate02GeneratorRaiting(lines) {
+        let raiting = '';
+        for (let col = 0; lines.length > 1; col++) {
+            const column = this.fetchColumnFromRows(lines, col);
+            const mcb = this.getMostCommonBit(column)
+            lines = lines.filter(this.columnHasCommonBit(col, mcb))
+            if (lines.length == 1) {
+                raiting = parseInt(lines[0], 2);
+                break;
+            }
+        }
+        return raiting;
+    }
+
+    fetchColumnFromRows(rows, n) {
+        const column = [];
+        for (let row = 0; row < rows.length; row++) {
+            column.push(rows[row].split('')[n]);
+        }
+        return column;
+    }
+
+    getMostCommonBit(column) {
+        const reducer = (prev, curr) => prev + ((curr > 0) ? 1 : -1);
+        const reducerValue = column.reduce(reducer, 0);
+        return ((reducerValue < 0) ? 0 : 1);
+    }
+
+    getLeastCommonBit(column) {
+        return (this.getMostCommonBit(column) ? 0 : 1)
+    }
+
+    columnHasCommonBit(col, bit) {
+        return (row) => (row.split('')[col] == bit)
     }
 
     solvePartOne() {
-        const report = Array(this.lines[0].length).fill(0);
-        for (const line of this.lines) {
-            line.split('').map((el, i) => {
-                report[i] += (Number(el) == 0) ? -1 : 1;
-            });
-        }
-        return this.calculateGammaRate([...report]) * this.calculateEpsilonRate([...report]);
+        return this.calculateGammaRate([...lines]) * this.calculateEpsilonRate([...lines]);
     }
 
     solvePartTwo() {
-        return null;
+        return this.calculate02GeneratorRaiting([...lines]) * this.calculateCO2ScrubberRaiting([...lines]);
     }
 
 }
